2b27e4c859b81c7bde0043ee6f09238b81c934a0
Vadsxd <vadim.sannikov.2018@mail.ru>
2022-12-15 11:35:45 +0300

    additional tests

--- src/test/java/com/shop/phoneshop/PhoneShopApplicationTests.java
+++ src/test/java/com/shop/phoneshop/PhoneShopApplicationTests.java
@@ -1,8 +1,19 @@
 package com.shop.phoneshop;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shop.phoneshop.domain.User;
+import com.shop.phoneshop.dto.JwtResponseDto;
 import com.shop.phoneshop.repos.ProductRepo;
+import com.shop.phoneshop.repos.UserProductRepo;
+import com.shop.phoneshop.repos.UserRepo;
 import com.shop.phoneshop.requests.AddProductRequest;
+import com.shop.phoneshop.requests.CartProductRequest;
+import com.shop.phoneshop.requests.auth.AuthRequest;
+import com.shop.phoneshop.security.jwt.JwtAuthentication;
+import com.shop.phoneshop.security.jwt.JwtProvider;
+import com.shop.phoneshop.security.jwt.JwtUtils;
+import com.shop.phoneshop.services.AuthService;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,21 +21,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.stream.LongStream;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class PhoneShopApplicationTests {
+    private final String userEmail = "vadim.sannikov.2018@mail.ru";
     private final MockMvc mockMvc;
     private final ProductRepo productRepo;
+    private final UserProductRepo userProductRepo;
+    private final AuthService authService;
+    private final JwtProvider jwtProvider;
+    private final UserRepo userRepo;
 
     @Autowired
-    public PhoneShopApplicationTests(MockMvc mockMvc, ProductRepo productRepo) {
+    public PhoneShopApplicationTests(
+            MockMvc mockMvc,
+            ProductRepo productRepo,
+            UserProductRepo userProductRepo,
+            AuthService authService,
+            JwtProvider jwtProvider,
+            UserRepo userRepo
+    ) {
         this.mockMvc = mockMvc;
         this.productRepo = productRepo;
+        this.userProductRepo = userProductRepo;
+        this.authService = authService;
+        this.jwtProvider = jwtProvider;
+        this.userRepo = userRepo;
     }
+
+    private JwtAuthentication authUser() {
+        AuthRequest request = new AuthRequest();
+        request.setUserEmail(userEmail);
+        request.setUserPassword("123");
+
+        JwtResponseDto jwtResponseDto = authService.authenticateUser(request);
+        Claims claims = jwtProvider.getAccessClaims(jwtResponseDto.getAccessToken());
+
+        return JwtUtils.getAuthentication(claims);
+    }
+
     @Test
     void checkEmptyCart() throws Exception {
         mockMvc
@@ -60,35 +101,45 @@ class PhoneShopApplicationTests {
     }
 
     @Test
-    void checkAllProducts() throws Exception {
-        int productsCount = productRepo.findAll().size();
-
-        mockMvc
-                .perform(get("/api/catalog"))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.categoryCount").value(productsCount));
-    }
-
-    //TODO доделать тест с проверкой корзины и всего каталога
-    @Test
-    void checkAllProductsWithCart() throws Exception {
-        int productsCount = productRepo.findAll().size();
-
-        AddProductRequest request = new AddProductRequest();
-        request.setProductId(3L);
+    void checkCartIncrease() throws Exception {
+        User user = userRepo.findByEmail(userEmail);
+        CartProductRequest request = new CartProductRequest();
+        request.setProductId(1L);
 
         ObjectMapper objectMapper = new ObjectMapper();
         byte[] bytes = objectMapper.writeValueAsBytes(request);
 
+        long cartCount = userProductRepo.findAllByUser(user).stream()
+                .flatMapToLong(a -> LongStream.of(a.getAmount()))
+                .sum();
+
         mockMvc
-                .perform(get("/api/catalog"))
+                .perform(put("/api/cart/addAmount").contentType(MediaType.APPLICATION_JSON)
+                        .content(bytes)
+                        .with(authentication(authUser())))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/cart").contentType(MediaType.APPLICATION_JSON)
+                .with(authentication(authUser())))
+                .andExpect(jsonPath("$.count").value(cartCount + 1));
+    }
+
+    @Test
+    void checkAllProductsWithCart() throws Exception {
+        int productsCount = productRepo.findAll().size();
+        User user = userRepo.findByEmail(userEmail);
+        long cartCount = userProductRepo.findAllByUser(user).stream()
+                .flatMapToLong(a -> LongStream.of(a.getAmount()))
+                .sum();
+
+        mockMvc
+                .perform(get("/api/catalog").contentType(MediaType.APPLICATION_JSON)
+                        .with(authentication(authUser())))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.categoryCount").value(productsCount))
-                .andExpect(jsonPath("$.cartCount").value(1L));
+                .andExpect(jsonPath("$.cartCount").value(cartCount));
     }
 
     @Test
